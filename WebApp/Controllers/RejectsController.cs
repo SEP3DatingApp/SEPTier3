8bc63d58028d28f6af78a0c6197d363e1ceef9a2
Mazils <arturasmaziliauskas2@gmail.com>
2020-05-11 23:44:51 +0200

    Added controllers for fisher,mathces and rejects

---
+++ WebApp/Controllers/RejectsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RejectsController : ControllerBase
+    {
+        private readonly WebAppContext _context;
+
+        public RejectsController(WebAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Rejects
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reject>>> GetRejects()
+        {
+            return await _context.Rejects.ToListAsync();
+        }
+
+        // GET: api/Rejects/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reject>> GetReject(int id)
+        {
+            var reject = await _context.Rejects.FindAsync(id);
+
+            if (reject == null)
+            {
+                return NotFound();
+            }
+
+            return reject;
+        }
+
+        // PUT: api/Rejects/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReject(int id, Reject reject)
+        {
+            if (id != reject.rejectId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reject).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RejectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rejects
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Reject>> PostReject(Reject reject)
+        {
+            _context.Rejects.Add(reject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReject", new { id = reject.rejectId }, reject);
+        }
+
+        // DELETE: api/Rejects/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Reject>> DeleteReject(int id)
+        {
+            var reject = await _context.Rejects.FindAsync(id);
+            if (reject == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rejects.Remove(reject);
+            await _context.SaveChangesAsync();
+
+            return reject;
+        }
+
+        private bool RejectExists(int id)
+        {
+            return _context.Rejects.Any(e => e.rejectId == id);
+        }
+    }
+}
---
+++ WebApp/Migrations/20200511213913_arturas.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class arturas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Users_userId",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "userType",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "sexPref",
+                table: "Users",
+                newName: "SexPref");
+
+            migrationBuilder.RenameColumn(
+                name: "picRef",
+                table: "Users",
+                newName: "PicRef");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Users",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "isActive",
+                table: "Users",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "gender",
+                table: "Users",
+                newName: "Gender");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Users",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "age",
+                table: "Users",
+                newName: "Age");
+
+            migrationBuilder.RenameColumn(
+                name: "username",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "userId",
+                table: "Matches",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "matchId",
+                table: "Matches",
+                newName: "MatchId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Matches_userId",
+                table: "Matches",
+                newName: "IX_Matches_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Users_UserId",
+                table: "Matches",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Users_UserId",
+                table: "Matches");
+
+            migrationBuilder.RenameColumn(
+                name: "SexPref",
+                table: "Users",
+                newName: "sexPref");
+
+            migrationBuilder.RenameColumn(
+                name: "PicRef",
+                table: "Users",
+                newName: "picRef");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Users",
+                newName: "isActive");
+
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "Users",
+                newName: "gender");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Users",
+                newName: "description");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Users",
+                newName: "age");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "username");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "password");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Matches",
+                newName: "userId");
+
+            migrationBuilder.RenameColumn(
+                name: "MatchId",
+                table: "Matches",
+                newName: "matchId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Matches_UserId",
+                table: "Matches",
+                newName: "IX_Matches_userId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "userType",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Users_userId",
+                table: "Matches",
+                column: "userId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
