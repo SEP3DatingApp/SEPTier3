389ca170d58813167ce9d5bf75e6a4269b62ef6f
Dalius Seminauskas <285039@VIAUC.DK>
2020-05-26 11:43:08 +0200

    ...

--- Sep3Tier3WithAuth/Models/FisherModel.cs
+++ Sep3Tier3WithAuth/Models/FisherModel.cs
@@ -2,9 +2,14 @@
 {
     public class FisherModel
     {
-        public string Id { get; set; }
+        public string Username { get; set; }
         public string FirstName { get; set; }
         public string Surname { get; set; }
-        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string SexPref { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
     }
 }
