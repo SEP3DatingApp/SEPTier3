f8c5ed4bce9429a00da117fcd2f31c0f2f1df720
Dalius Seminauskas <285039@VIAUC.DK>
2020-05-31 11:53:04 +0200

    Returns persons id according to their sex pref

--- Sep3Tier3WithAuth/Models/FisherInfoForMatches.cs
+++ Sep3Tier3WithAuth/Models/FisherInfoForMatches.cs
@@ -2,12 +2,6 @@
 {
     public class FisherInfoForMatches
     {
-        public string FirstName { get; set; }
-        public string PicRef { get; set; }
-        public string Gender { get; set; }
-        public int PersonSexualityId { get; set; }
-        public string PersonalSexualityName { get; set; }
-        public int Age { get; set; }
-        public string Description { get; set; }
+        public int Id { get; set; }
     }
 }
